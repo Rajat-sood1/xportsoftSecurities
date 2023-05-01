3c029a42c2db9ad25019d0f1a49a5419cf927f38
Dev.Rajat <84976487+Rajat-sood1@users.noreply.github.com>
2023-05-01 19:27:49 +0530

    Update index.js

--- src/components/tableRows/index.js
+++ src/components/tableRows/index.js
@@ -35,7 +35,7 @@ const TableRow = ({id, title, duration, progress}) =>{
                     {loggedInUser.isCompleted?<td className="w-8 border sts"> &#10003;</td>:<td className="w-8 border sts-r"> &#128473;</td>}
                     
                     <td className="w-25 border" >
-                    <button className={loggedInUser.isCompleted?'btn':'btn restricted'} onClick={openModule(id)}>Review</button>
+                         <NavLink to={`/${module}/${id}`}> <button className={loggedInUser?'btn':'btn restricted'}>Review</button></NavLink>
                     </td>
               </tr>
           </>
